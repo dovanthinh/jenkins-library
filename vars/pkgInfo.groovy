582caafac7808a38077638dadffd243cd52e454a
thinhdv <thinhdv90@gmail.com>
2019-01-10 16:55:25 +0700

    fix jobname in pkgInfo.groovy

--- vars/pkgInfo.groovy
+++ vars/pkgInfo.groovy
@@ -15,11 +15,12 @@ def getPkgInfo(pkg) {
 
 // The function will check the condition that the pkg can be deployed on server?
 def checkPkgPermission(pkgName, srv) {
+    String jobName = env.JOB_NAME
     def pkgAllowList
     def allowFlag = 0
     try {
-        pkgAllowList = jenkinsPkg.allowList.get("ADM-tools").get("PKGALLOWLIST").tokenize(" ")
-        srvAllowList = jenkinsPkg.allowList.get("ADM-tools").get("SRVALLOWLIST").tokenize(" ")
+        pkgAllowList = jenkinsPkg.allowList.get(jobName).get("PKGALLOWLIST").tokenize(" ")
+        srvAllowList = jenkinsPkg.allowList.get(jobName).get("SRVALLOWLIST").tokenize(" ")
     } catch(exp) {
         error("Allowlist for this job is NOT set")
     }
