c331bc36c2cabfdcc1feef1a6fb186bcc20557c7
thinhdv <thinhdv90@gmail.com>
2019-01-23 15:34:54 +0700

    add fix telegramNotify.groovy

--- vars/telegramNotify.groovy
+++ vars/telegramNotify.groovy
@@ -1,10 +1,10 @@
-def notify(JENKINSJOB, PKG, SERVER, STATUS="SUCCESS") {
+def notify(JENKINSJOB, PKG, SERVER, STATUS) {
    BOT_TOKEN="744656308:AAE50sZTFOokn7f1hLTg5JiX2VBUQr14drA"
    MESSAGE = """*JenkinsJob: $JENKINSJOB*
    ```
-   DeployedPkg:		$PKG$
-   OnServer:  		$SERVER$
-   Status:			$STATUS
+   DeployedPkg:	$PKG$
+   OnServer: $SERVER$
+   Status: $STATUS
    ```"""
    JSON = '{"chat_id": "@adm_notify", "disable_notification": false, "text": "' + "${MESSAGE}" + '"}'
    def proc = ["curl", "-X", "POST", "-H", "Content-Type: application/json", "-d", "${JSON}",  "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage"].execute()
